8e24ccdd015dd9a7ab19394a2bc52bea84e5d0f9
Ananya Shrivastava <ananaya.kv@gmail.com>
2020-09-22 17:18:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,12 +6,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2_instance" {
     ami = "${var.ami_id}"
+    instance_type = "${var.instance_type}"
 
 }
 
-module "ec2_instance" {
-  source = "git::https://github.com/ananya-shrivastava/ANANYA_AWS_EC2"
-  instance_type = "${var.instance_type}"
-  ami_id = "${var.ami_id}"
-  
-}
